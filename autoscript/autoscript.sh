8d643a4215a88933449fa9b37591f6c736e715d8
count-fuzzball <50670715+count-fuzzball@users.noreply.github.com>
2019-05-16 08:55:39 +0100

    Update autoscript.sh

--- autoscript/autoscript.sh
+++ autoscript/autoscript.sh
@@ -9,7 +9,7 @@ sudo usermod -a -G bluetooth void
 sudo rm /var/service/sshd
 sudo ln -s /etc/sv/dbus /var/service/
 sudo ln -s /etc/sv/bluetoothd /var/service/
-sudo ln -s /etc/sv/xdm /var/service/
+
 
 sudo mkdir /etc/sv/BTpwr
 cat <<EOF > ./pwrrun 
@@ -24,15 +24,21 @@ sudo mv ./pwrrun /etc/sv/BTpwr/run
 sleep 2
 sudo ln -s /etc/sv/BTpwr /var/service/
 
-cp -rv home/conkyconfigs /home/$USER
+cp -r home/conkyconfigs /home/$USER
 mkdir /home/$USER/.config
-cp -rv home/dotfiles/config/* /home/$USER/.config/
+cp -r home/dotfiles/config/* /home/$USER/.config/
 mkdir /home/$USER/.fluxbox
-cp -rv home/dotfiles/fluxbox/* /home/$USER/.fluxbox/
+cp -r home/dotfiles/fluxbox/* /home/$USER/.fluxbox/
 mkdir /home/$USER/.fonts
-cp -rv home/dotfiles/fonts/* /home/$USER/.fonts/
+cp -r home/dotfiles/fonts/* /home/$USER/.fonts/
 
-cp -v home/dotfiles/bash_profile /home/$USER/.bash_profile
-cp -v home/dotfiles/bashrc /home/$USER/.bashrc
-cp -v home/dotfiles/gtkrc-2.0 /home/$USER/.gtkrc-2.0
-cp -v home/dotfiles/xsession /home/$USER/.xsession
+cp home/dotfiles/bash_profile /home/$USER/.bash_profile
+cp home/dotfiles/bashrc /home/$USER/.bashrc
+cp home/dotfiles/gtkrc-2.0 /home/$USER/.gtkrc-2.0
+cp home/dotfiles/xsession /home/$USER/.xsession
+
+sudo ln -s /etc/sv/xdm /var/service/
+
+echo Rebooting now
+sleep 5
+sudo reboot
